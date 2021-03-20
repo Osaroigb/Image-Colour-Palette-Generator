41f647b71ec390a79b45f7804d73710f70894d3e
Osaro <emmyigbinovia@gmail.com>
2021-03-20 12:55:14 +0100

    Set environment variable in heroku

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 # setup flask application
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config['UPLOAD_PATH'] = "static"
 
 
